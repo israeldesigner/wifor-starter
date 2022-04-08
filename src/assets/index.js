f8540f47a141a3a71d606bbe8b8ba28df3460e75
israel <israeldesignerce@outlook.com>
2022-04-08 17:46:17 -0300

    fontawsome6

--- src/assets/index.js
+++ src/assets/index.js
@@ -6,6 +6,7 @@ import './css/glowCookies.css'
 import './css/font-awesome.css'
 import './css/simple-line.min.css'
 import './css/aos.css'
+import '../../node_modules/@fortawesome/fontawesome-free/css/regular.css'
 
 //import scss
 import './scss/index.scss'
